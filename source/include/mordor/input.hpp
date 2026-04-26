63eb5bec8d45b293a7c8705d0415cc2a2de776a4
Stephen Whipp <stephenwhippuk@gmail.com>
2026-04-26 01:06:36 +0100

    P1-04: add input action binding layer for camera controls

---
+++ source/include/mordor/input.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <initializer_list>
+#include <vector>
+
+namespace mordor {
+
+enum class InputAction : std::size_t
+{
+    PanLeft = 0,
+    PanRight,
+    PanUp,
+    PanDown,
+    RotateLeft,
+    RotateRight,
+    ZoomIn,
+    ZoomOut,
+    Count
+};
+
+class InputBindings
+{
+public:
+    InputBindings();
+
+    void set_bindings(InputAction action, std::initializer_list<int> keys);
+
+    template <typename IsKeyDownFn>
+    bool is_action_active(InputAction action, IsKeyDownFn&& is_key_down) const
+    {
+        const auto& keys = m_bindings[static_cast<std::size_t>(action)];
+        for (const int key : keys)
+        {
+            if (is_key_down(key))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+private:
+    std::array<std::vector<int>, static_cast<std::size_t>(InputAction::Count)> m_bindings{};
+};
+
+} // namespace mordor
---
+++ source/src/input.cpp
@@ -0,0 +1,38 @@
+#include "mordor/input.hpp"
+
+#if MORDOR_HAS_GLFW
+#include <GLFW/glfw3.h>
+#endif
+
+namespace mordor {
+
+InputBindings::InputBindings()
+{
+#if MORDOR_HAS_GLFW
+    set_bindings(InputAction::PanLeft, {GLFW_KEY_A, GLFW_KEY_LEFT});
+    set_bindings(InputAction::PanRight, {GLFW_KEY_D, GLFW_KEY_RIGHT});
+    set_bindings(InputAction::PanUp, {GLFW_KEY_W, GLFW_KEY_UP});
+    set_bindings(InputAction::PanDown, {GLFW_KEY_S, GLFW_KEY_DOWN});
+    set_bindings(InputAction::RotateLeft, {GLFW_KEY_Q});
+    set_bindings(InputAction::RotateRight, {GLFW_KEY_E});
+    set_bindings(InputAction::ZoomIn, {GLFW_KEY_R});
+    set_bindings(InputAction::ZoomOut, {GLFW_KEY_F});
+#else
+    set_bindings(InputAction::PanLeft, {});
+    set_bindings(InputAction::PanRight, {});
+    set_bindings(InputAction::PanUp, {});
+    set_bindings(InputAction::PanDown, {});
+    set_bindings(InputAction::RotateLeft, {});
+    set_bindings(InputAction::RotateRight, {});
+    set_bindings(InputAction::ZoomIn, {});
+    set_bindings(InputAction::ZoomOut, {});
+#endif
+}
+
+void InputBindings::set_bindings(InputAction action, std::initializer_list<int> keys)
+{
+    auto& action_keys = m_bindings[static_cast<std::size_t>(action)];
+    action_keys.assign(keys.begin(), keys.end());
+}
+
+} // namespace mordor
